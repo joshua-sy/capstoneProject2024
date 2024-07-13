8676f83ab87f87463f1fdcdc7c50231b3eb39421
Samiksha Anirudh <z5346866@unsw.edu.au>
2024-07-14 03:00:14 +1000

    Successfully tested GET endpoint

---
+++ api/Models/SvfResult.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace api.models
+{
+    public class SvfResult
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Output { get; set; } = string.Empty;
+        public List<DotGraph> Graphs { get; set; } = [];
+        public string Llvm { get; set; } = string.Empty;
+    }
+
+}
