972a003c9f7c5bca9d30da5c17efc506ea5dedbd
Samiksha Anirudh <samiksha.anirudh@gmail.com>
2024-07-30 00:23:40 +1000

    Clean up changes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e111660fb07c2f8930c10e20a16aac7f7ba7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d7bb7cc5b7b2714165c4f3be9140c08bb8f72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
