42c08a5bf159dde3d367ff426c09a9baedc8ca30
Samiksha Anirudh <samiksha.anirudh@gmail.com>
2024-07-30 18:19:03 +1000

    Resolved dll changes and node modules

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2922400e02618d7b0046d57db2930ba5ae809cea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3885201e0aa5059429b0cfbccf11bc27f15687d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
